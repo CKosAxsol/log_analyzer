99f542ed9c3711bed22f17fe98f1a63a7dae6bed
C. Kossner-W. <ckossnerwischanowski@axsol.eu>
2026-04-28 13:49:53 +0200

    start plotter batch file added

---
+++ start_csv_plotter.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d "%~dp0"
+
+if not exist ".venv" (
+    py -m venv .venv
+)
+
+call .venv\Scripts\activate.bat
+
+python -m pip install --upgrade pip
+
+python csv_plotter.py
+
+pause
